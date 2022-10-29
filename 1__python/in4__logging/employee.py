4b71a75aeb02c760539a51886d8ad351b2244019
Suryansh <suryanshgrover1999@gmail.com>
2022-10-29 13:58:18 +0530

    logging explored in depth

---
+++ 1__python/in4__logging/employee.py
@@ -0,0 +1,52 @@
+import logging as lg
+
+logger = lg.getLogger(__name__) # Specifying a logger name!
+logger.setLevel(lg.INFO)
+# Now to set the basicConfig for this logger instead of using default config of the `root`` logger, 
+# we have to create a fileHandler for this logger.
+handler = lg.FileHandler('employee (new).log')
+# now need to add this handler to the desired logger
+logger.addHandler(handler)
+
+# The same goes for the formatter. All the configuration that needs to be done for the `logger`, gotta be added
+# or set to its handler.
+formatter = lg.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s')
+handler.setFormatter(formatter)
+
+# NOTE: We can also set the level (that we initially set to the logger itself) to the filehandler, making the file 
+# itself to filter all the logs accordingly.
+
+# lg.basicConfig(filename='employee.log', level=lg.INFO,
+#                     format='%(name)s %(asctime)s %(levelname)s %(message)s')
+
+# NOTE: Since we haven't specified a logger name, so by default the logger name is `root`. 
+
+
+class Employee:
+    """A sample Employee class"""
+
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+        logger.info('Created Employee: {} - {}'.format(self.fullname, self.email))
+
+    @property
+    def email(self):
+        logger.info('{}{}@email.com'.format(self.first, self.last))
+        return '{}{}@email.com'.format(self.first, self.last)
+
+    @property
+    def fullname(self):
+        logger.info('{} {}'.format(self.first, self.last))
+        return '{} {}'.format(self.first, self.last)
+
+
+emp_1 = Employee('suryansh', 'grover1999')
+# emp_1.email
+
+emp_2 = Employee('spidey', 'styles')
+# emp_2.fullname
+
+emp_3 = Employee('sherlock', 'holmes1854')
+# emp_3.email
---
+++ 1__python/in4__logging/log_sample.py
@@ -0,0 +1,72 @@
+import logging as lg
+import employee
+
+logger = lg.getLogger(__name__)
+logger.setLevel(lg.INFO)
+f_handler = lg.FileHandler('sample (new).log')
+f_handler.setFormatter(lg.Formatter("%(asctime)s %(name)s %(message)s"))
+logger.addHandler(f_handler)
+
+## Now, even after creating a separate filehandler, logs are getting stored to the file created by the basicConfig
+## as well, thus, we have to NOT SET the basicConfig or there's no point of creating a separate handler beacuase 
+## makes no sense to store logs simultaneously into two files.
+# lg.basicConfig(filename='sample.log', level=lg.INFO, format='%(name)s %(asctime)s %(levelname)s %(message)s')
+
+"""
+NOTE: Now that I've imported the 'employee' module, 'sample.log' isn't getting created and all the logs are being
+stored to the 'employee.log' log file.
+
+REASON: Because by the time logging configuration of this file gets configured, configuration of `epmloyee` module
+has already been configured. 
+"""
+
+"""
+DEBUG: Detailed information, typically of interest only when diagnosing problems.
+
+INFO: Confirmation that things are working as expected.
+
+WARNING (default level): An indication that something unexpected happened, or indicative of some problem in the near future 
+(e.g. ‘disk space low’). The software is still working as expected.
+
+--> default level means here by default only warnings and levels above warning that are error and critical will 
+logged into the console.
+
+ERROR: Due to a more serious problem, the software has not been able to perform some function.
+
+CRITICAL: A serious error, indicating that the program itself may be unable to continue running.
+"""
+
+def add(x, y):
+    """Add Function"""
+    return x + y
+
+
+def subtract(x, y):
+    """Subtract Function"""
+    return x - y
+
+
+def multiply(x, y):
+    """Multiply Function"""
+    return x * y
+
+
+def divide(x, y):
+    """Divide Function"""
+    return x / y
+
+
+num_1 = 21
+num_2 = 19
+
+add_result = add(num_1, num_2)
+logger.info('Add: {} + {} = {}'.format(num_1, num_2, add_result))
+
+sub_result = subtract(num_1, num_2)
+logger.info('Sub: {} - {} = {}'.format(num_1, num_2, sub_result))
+
+mul_result = multiply(num_1, num_2)
+logger.info('Mul: {} * {} = {}'.format(num_1, num_2, mul_result))
+
+div_result = divide(num_1, num_2)
+logger.info('Div: {} / {} = {}'.format(num_1, num_2, div_result))
